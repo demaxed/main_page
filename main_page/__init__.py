a646b3228e42a00322df5c02c8450ba6e2c84cb9
mdemyanenko <mdemyanenko@htsts.ru>
2020-06-15 16:58:16 +0300

    initial commit

---
+++ main_page/__init__.py
@@ -0,0 +1,18 @@
+"""
+Dockstring
+"""
+__author__ = 'demaxed'
+__maintainer__ = __author__
+
+__email__ = 'email'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
